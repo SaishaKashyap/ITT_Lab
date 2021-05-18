c0e55b7a91751ca14ac23af296156764fb65cd16
Saisha Kashyap <saishakashyap8@gmail.com>
2021-05-18 20:27:20 +0530

    Python-1

---
+++ Lab5/1a.py
@@ -0,0 +1,15 @@
+a= int(input("Enter the first number:") )
+op = input("Enter operation:")
+b = int(input("Enter the second number:"))
+
+if op == '+':
+    print("a+b=",a+b) 
+elif op == '-':
+    print("a-b=",a-b)
+elif op == '*':
+    print("a*b=",a*b)
+elif op == '/':
+    print("a/b=",a/b)
+
+
+    
